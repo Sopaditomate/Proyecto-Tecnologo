8333c5aaff3b3681ea69fbaf2c975c0b14cf4ffa
Sopaditomate <darkaz2017@gmail.com>
2025-06-19 11:35:45 -0500

    Se cambio la orientacion de sp a views, al menos de momento. Hay un error en cuanto al llamado de los datos desde varias partes. Falta cambiar cosas

--- src/models/CartModel.js
+++ src/models/CartModel.js
@@ -1,19 +1,38 @@
 import pool from "../config/db.js";
 
   class CartModel {
+    //este no sirve
     async getCartByUserId(userId) {
       const [rows] = await pool.query(
-        "CALL sp_get_cart_by_user(?)",
+        "SELECT * FROM vw_active_cart_by_user WHERE id_usuario = (?)",
         [userId]
       );
       return rows[0] || null;
     }
 
+    // async getCartByUserId(userId) {
+    //   const [rows] = await pool.query(
+    //     "CALL sp_get_cart_by_user(?)",
+    //     [userId]
+    //   );
+    //   console.log(rows)
+    //   return rows[0] || null;
+    // }
+
+    // async getCartByUserId(userId) {
+    //   const [rows] = await pool.query(
+    //     "SELECT id_cart as id, id_user as id_usuario, items, updated_at as actualizado FROM cart WHERE id_user = ?",
+    //     [userId]
+    //   );
+    //   return rows[0] || null;
+    // }
+
+/////////este esta bn
     async saveCart(userId, items) {
       const itemsJson = JSON.stringify(items);
       await pool.query("CALL sp_save_cart(?, ?)", [userId, itemsJson]);
     }
-
+/////////este esta por ver
     async clearCart(userId) {
       await pool.query("CALL sp_clear_cart(?)", [userId]);
     }
