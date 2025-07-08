e7da8040084799e99fa804680044e6db39169f6b
Sopaditomate <darkaz2017@gmail.com>
2025-07-07 22:14:51 -0500

    Se logro hacer la carga masiva para inventario, falta productos y terminar los estilos coherentes

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -2,6 +2,12 @@ import express from 'express';
 import InventoryController from '../controllers/InventoryController.js';
 //import { verifyToken, isAdmin } from "../middleware/authMiddleware.js";
 
+//para el cargue masivo
+import multer from 'multer';
+const storage = multer.memoryStorage(); // o diskStorage si deseas guardar el archivo
+const upload = multer({ storage });
+
+
 const router = express.Router();
 
 // Todas las rutas requieren autenticación y rol de administrador
@@ -19,6 +25,9 @@ router.get("/grafica/:id(\\d+)", InventoryController.getInventoryByUnit);
 
 // Operaciones CRUD
 router.post("/nuevo", InventoryController.agregarNuevoInsumo);
+//para la carga masiva
+router.post("/cargar", upload.single('file'), InventoryController.cargarMasivaDesdeCSV);
+
 //El update inventario incluye el llamado a dos modelos
 router.put('/:id(\\d+)', InventoryController.updateInventario);
 router.put('/materia/:id(\\d+)', InventoryController.updateMateriaPrimaPorInventario);
