0b3189be0106a85acec5e3adf65082ba37405b63
ZGuillerX <espriellag8@gmail.com>
2025-08-04 16:35:03 -0500

    no se

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  resolve: {
-  },
+  resolve: {},
   optimizeDeps: {
     exclude: ["bcrypt"],
   },
