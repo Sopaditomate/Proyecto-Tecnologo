3f13eb7db84ec6b14c661d6fad00023d91bde808
ZGuillerX <espriellag8@gmail.com>
2025-09-18 09:39:51 -0500

    no se pueden enviar los correos desde railway

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -2,12 +2,13 @@ import nodemailer from "nodemailer";
 
 export async function sendEmail(to, subject, text, html) {
   const transporter = nodemailer.createTransport({
-    host: "smtp.elasticemail.com",
-    port: 2525,
-    secure: false,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_API_KEY,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
   await transporter.sendMail({
