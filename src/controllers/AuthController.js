12b921c7a054290a5f0afad524a24b8ee109e742
ZGuillerX <espriellag8@gmail.com>
2025-09-21 17:01:42 -0500

    ajustando las cookies y el http only

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -96,7 +96,7 @@ class AuthController {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV,
-        sameSite: "none",
+        sameSite: "lax",
         maxAge: 60 * 60 * 1000, // 1 hora
       });
 
